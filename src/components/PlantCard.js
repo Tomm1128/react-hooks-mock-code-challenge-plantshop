865adfba37985d9ec8c07a02680245f9e5beffad
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-21 10:56:38 -0400

    Add edit price button and input

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -2,20 +2,40 @@ import React, { useState } from "react"
 
 function PlantCard({ plant: { name, image, price } }) {
   const [inStock, setStock] = useState(true)
+  const [isHidden, setHidden] = useState(true)
+  const [newPrice, setNewPrice] = useState(0)
 
   const updateStock = () => setStock(!inStock)
+  const showEditPrice = () => setHidden(!isHidden)
+
+  const updatePrice = (event) => {
+    showEditPrice()
+  }
 
   return (
     <li className="card">
       <img src={image} alt={"plant name"} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
+      <input
+        type="number"
+        step="0.01"
+        placeholder="Update Price..."
+        hidden={isHidden}
+        value={newPrice}
+        onChange={(event) => setNewPrice(event.target.value)}
+      ></input>
       {inStock ? (
         <button className="primary" onClick={updateStock}>
           In Stock
         </button>
       ) : (
         <button onClick={updateStock}>Out of Stock</button>
+      )}
+      {isHidden ? (
+        <button onClick={showEditPrice}>Update Price</button>
+      ) : (
+        <button onClick={updatePrice}>Save Price</button>
       )}
     </li>
   )
